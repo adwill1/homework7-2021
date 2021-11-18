5431d30a4fe895cb6d8ef12490b4b74b6d6b46b7
adwill1 <59946859+adwill1@users.noreply.github.com>
2021-11-17 22:31:18 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -16,43 +16,26 @@ document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
 });
 
-var slow_count = 0.95;
+
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slower");
-	video.playbackRate = slow_count;
-	console.log(slow_count);
-	slow_count = 0.95*slow_count;
-	return slow_count;
+	video.playbackRate = video.playbackRate * 0.95;
+	console.log(video.playbackRate);
 })
 
-var fast_count = slow_count;
 document.querySelector("#faster").addEventListener("click", function() {
 	console.log("Faster");
-	fast_count = 1.05*slow_count;
-	video.playbackRate = fast_count;
-	fast_count = 1.05*fast_count;
-	console.log(fast_count);
+	video.playbackRate = video.playbackRate / 0.95;
+	console.log(video.playbackRate);
 })
 
-var time = 0 + 15;
 document.querySelector("#skip").addEventListener("click", function() {
-	if (time == 75){
-		time = 0;
-		console.log(time);
+	if (video.currentTime < video.duration - 15){
+		video.currentTime += 15;
 	}else{
-		console.log("Time");
-		video.currentTime = time;
-		console.log(time);
-		time = time + 15;
+		video.currentTime = 0;
 	}
-	// console.log("Time");
-	// video.currentTime = time;
-	// time = time + 15;
-	// console.log(video.currentTime);
-	// if (video.currentTime == 90){
-	// 	video.currentTime = 0
-	// 	console.log(video.currentTime)
-	// }
+	console.log(video.currentTime);
 })
 
 
